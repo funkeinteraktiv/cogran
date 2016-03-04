7077b86fecacef06da3067b243dd8fbc0646ac5e
Christopher Möller <chrtze@gmail.com>
2016-03-04 16:07:58 +0100

    add more tests for aggregation module

--- test/aggregate-test.js
+++ test/aggregate-test.js
@@ -1,14 +1,24 @@
 'use strict';
 
 const Expect = require('expect');
+const Shapefile = require('shapefile');
 const Aggregate = require('../lib/aggregate');
-const Async = require('async');
 
-const aggConfigs = {
+const config = {
   basic: { 
     input: './test/data/wahl/wahl.shp', 
     target: './test/data/mieten/mieten.shp',
     mode: 'sum',
+    attr: 'BRFW'
+  },
+
+  missingTarget: {
+    input: './test/data/wahl/wahl.shp' 
+  },
+
+  missingMode: { 
+    input: './test/data/wahl/wahl.shp', 
+    target: './test/data/mieten/mieten.shp',
     attr: 'BRFW'
   }
 }
@@ -21,7 +31,7 @@ describe('aggregation module', () => {
 
     beforeEach((cb) => {
       
-      Aggregate(aggConfigs.basic, (res) => {
+      Aggregate(config.basic, (res) => {
         returnValue = res;
         cb();
       });
@@ -41,6 +51,58 @@ describe('aggregation module', () => {
     
     });
   
+  });
+
+  describe('output data', () => {
+
+    let inputData, outputData;
+
+    beforeEach((cb) => {
+
+      Aggregate(config.basic, (res) => {
+        outputData = res;
+
+        Shapefile.read(config.basic.target, (err,inputJson) => {
+          inputData = inputJson;
+          cb();
+        });
+        
+      });
+
+    });
+
+
+    it('should have the same number of features as the target file',() => {
+
+      Expect(inputData.features.length).toBe(outputData.features.length);
+
+    });
+
+  });
+
+  describe('error handling', () => {
+
+    let returnValue;
+
+    beforeEach((cb) => {
+
+      Aggregate(config.missingMode, (res) => {
+        returnValue = res;
+        cb();
+      });
+
+    });
+
+    it('should output an error if target parameter is missing', () => {
+      let rV = Aggregate(config.missingTarget);
+      Expect(typeof rV).toBe('string');
+    });
+
+    it('should default to sum if mode is missing', () => {
+      Expect(typeof returnValue).toBe('object');
+      Expect(returnValue.type).toBe('FeatureCollection');
+    });
+
   });
 
 });
