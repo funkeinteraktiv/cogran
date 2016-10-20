402b4b902fea9ec9cdc5431152e74821eac2b6f6
IngaSchl <inga.schlegel@hcu-hamburg.de>
2016-10-20 13:33:24 +0200

    updated expect values

--- test/linear-regression-test.js
+++ test/linear-regression-test.js
@@ -58,8 +58,89 @@ describe('linear regression [hierarchical, absolute]', () => {
     });
 
     it('should calculate the output values correctly',() => {
-      Expect(outputData.features[0].properties[Config.hierarch_abs.attr]).toBeGreaterThan(20).toBeLessThan(21);
-      Expect(outputData.features[1].properties[Config.hierarch_abs.attr]).toBeGreaterThan(52).toBeLessThan(53);
+      Expect(outputData.features[0].properties[Config.hierarch_abs.attr]).toBeGreaterThan(16).toBeLessThan(17);
+      Expect(outputData.features[1].properties[Config.hierarch_abs.attr]).toBeGreaterThan(56).toBeLessThan(57);
+    });
+
+  });
+
+});
+
+describe('linear regression [hierarchical, relative]', () => {
+
+  describe('output data', () => {
+
+    let inputData, outputData;
+
+    beforeEach((cb) => {
+
+      Aggregate(Config.hierarch_rel, (res) => {
+        outputData = res;
+        FileLoader.readGeoJson([Config.hierarch_rel.target], (err, res) => {
+          inputData = res[0];
+          cb();
+        });
+      });
+
+    });
+
+    it('should calculate the output values correctly',() => {
+      Expect(outputData.features[0].properties[Config.hierarch_rel.attr]).toBeGreaterThan(8).toBeLessThan(9);
+      Expect(outputData.features[1].properties[Config.hierarch_rel.attr]).toBeGreaterThan(22).toBeLessThan(23);
+    });
+
+  });
+
+});
+
+describe('linear regression [nonhierarchical, absolute]', () => {
+
+  describe('output data', () => {
+
+    let inputData, outputData;
+
+    beforeEach((cb) => {
+
+      Aggregate(Config.nonHierarch_abs, (res) => {
+        outputData = res;
+        FileLoader.readGeoJson([Config.nonHierarch_abs.target], (err, res) => {
+          inputData = res[0];
+          cb();
+        });
+      });
+
+    });
+
+    it('should calculate the output values correctly',() => {
+      Expect(outputData.features[0].properties[Config.nonHierarch_abs.attr]).toBeGreaterThan(4).toBeLessThan(5);
+      Expect(outputData.features[1].properties[Config.nonHierarch_abs.attr]).toBeGreaterThan(60).toBeLessThan(61);
+    });
+
+  });
+
+});
+
+describe('linear regression [nonhierarchical, relative]', () => {
+
+  describe('output data', () => {
+
+    let inputData, outputData;
+
+    beforeEach((cb) => {
+
+      Aggregate(Config.nonHierarch_rel, (res) => {
+        outputData = res;
+        FileLoader.readGeoJson([Config.nonHierarch_rel.target], (err, res) => {
+          inputData = res[0];
+          cb();
+        });
+      });
+
+    });
+
+    it('should calculate the output values correctly',() => {
+      Expect(outputData.features[0].properties[Config.nonHierarch_rel.attr]).toBeGreaterThan(3).toBeLessThan(4);
+      Expect(outputData.features[1].properties[Config.nonHierarch_rel.attr]).toBeGreaterThan(32).toBeLessThan(33);
     });
 
   });
